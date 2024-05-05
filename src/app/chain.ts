43fb9222905a92d24bdab39c2024bf85c88f9fd4
Ethix Lucifer <ethixlucifer@gmail.com>
2024-05-05 22:06:21 +0530

    added env file

--- src/app/chain.ts
+++ src/app/chain.ts
@@ -1,5 +1,6 @@
 import { defineChain } from "thirdweb";
 import { polygonAmoy } from "thirdweb/chains";
-// import { baseSepolia } from "thirdweb/chains";
 
+// import { baseSepolia } from "thirdweb/chains";
+const CLI = process.env.client_id;
 export const chain = defineChain( polygonAmoy );
